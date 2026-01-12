ee89fe13eae2663a716696a3df3a0a64f7e66ab6
Mingyu Jeong <159212138+RFA0608@users.noreply.github.com>
2026-01-13 07:27:23 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <gmpxx.h>
 
 #include "seal/seal.h"
-#include <vector>
-
-#include "Struct.h"
+#include "RLWE.h"
 
 using namespace std;
 using namespace seal;
@@ -27,8 +26,8 @@ int main()
     parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, plain_modulus));
     SEALContext context(parms);
 
-    mpz_t mod;
-    mpz_init_set_str(mod, to_string(parms.plain_modulus().value()).c_str(), 10);
+    mpz_t plain_mod;
+    mpz_init_set_str(plain_mod, to_string(parms.plain_modulus().value()).c_str(), 10);
 
     // set batch encoder 
     BatchEncoder batch_encoder(context);
@@ -52,19 +51,32 @@ int main()
     poly* p3 = new poly(poly_degree);
 
     poly_set(packed_data, p1);
-    poly_set(packed_data, p2);
 
-    // poly_add(p1, p2, p3);
-    poly_mul(p1, p2, p3);
-    poly_mod(p3, mod, p3);
+    mpz_t* cipher_mod = find_ntt_prime(p1->size, 110);
+    mpz_out_str(stdout, 10, *cipher_mod);
+    cout << endl;
+    cout <<"is prime : " << is_prime(*cipher_mod) << endl;
 
-    mpz_t rop;
-    mpz_init(rop);
-    find_primitive_root(mod, rop);
+    cipher* c = new cipher(p1->size, plain_mod, *cipher_mod);
+    poly* sk = secret_key(p1->size);
 
-    poly_mul_ntt(p1, p2, mod, rop, p3);
+    encrypt(p1, sk, c);
+    decrypt(c, sk, p2);
+    poly_get(p2, result_data);
+    // poly_set(packed_data, p2);
 
-    poly_get(p3, result_data);
+    // // poly_add(p1, p2, p3);
+    // poly_mul(p1, p2, p3);
+    // poly_mod(p3, mod, p3);
+
+    // mpz_t rop;
+    // mpz_init(rop);
+    // find_primitive_root(mod, rop);
+    // int r = is_prime(mod);
+
+
+    // poly_mul_ntt(p1, p2, mod, rop, p3);
+    // poly_get(p3, result_data);
 
     vector<int64_t> repod_matrix(slot_count, 0LL);
     batch_encoder.decode(result_data, repod_matrix);
@@ -74,9 +86,14 @@ int main()
         cout << repod_matrix[i] << endl;
     }
 
-    p1->~poly();
-    p2->~poly();
-    p3->~poly();
+    delete p1;
+    delete p2;
+    delete p3;
+    delete c;
+    delete sk;
+
+    mpz_clears(plain_mod, *cipher_mod, NULL);
+    delete cipher_mod;
 
     return 0;
 }
